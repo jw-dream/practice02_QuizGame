c2896ec6bf5c1590d4077e7c4d41afb694dfec66
jw-dream <127647366+jw-dream@users.noreply.github.com>
2023-03-17 09:45:51 +0900

    Update Quiz.sol

--- src/Quiz.sol
+++ src/Quiz.sol
@@ -10,10 +10,17 @@ contract Quiz{
       uint max_bet;
    }
     
+    address owner = msg.sender;
     mapping(address => uint256)[] public bets;
+    uint quiz_index = 0;
     uint public vault_balance;
+    mapping(uint => Quiz_item) quiz_list;
+    mapping(uint => string) quiz_answer_list;
+    mapping(address => uint) winner_balance_list;
+    
 
     constructor () {
+        owner = msg.sender;
         Quiz_item memory q;
         q.id = 1;
         q.question = "1+1=?";
@@ -23,25 +30,58 @@ contract Quiz{
         addQuiz(q);
     }
 
+
     function addQuiz(Quiz_item memory q) public {
+        require(msg.sender == owner);
+      
+        quiz_answer_list[q.id] = q.answer;
+        q.answer = "";
+
+        quiz_index++;
+
+        quiz_list[quiz_index] = q;
     }
 
     function getAnswer(uint quizId) public view returns (string memory){
+        return quiz_answer_list[quizId];
     }
 
     function getQuiz(uint quizId) public view returns (Quiz_item memory) {
+        return quiz_list[quizId];
     }
 
     function getQuizNum() public view returns (uint){
+        return quiz_index;
     }
     
     function betToPlay(uint quizId) public payable {
+        require(quiz_list[quizId].min_bet <= msg.value);
+        require(quiz_list[quizId].max_bet >= msg.value);
+        bets.push();
+        bets[quizId-1][msg.sender] += msg.value;
     }
 
     function solveQuiz(uint quizId, string memory ans) public returns (bool) {
+        if(keccak256(abi.encode(quiz_answer_list[quizId])) == keccak256(abi.encode(ans))){
+            winner_balance_list[msg.sender] += bets[quizId-1][msg.sender] * 2; 
+            return true;
+        }
+        else{
+            vault_balance += bets[quizId-1][msg.sender];
+            bets[quizId-1][msg.sender] = 0;
+            return false;
+        }
+
     }
 
     function claim() public {
+        uint money = winner_balance_list[msg.sender];
+        winner_balance_list[msg.sender] = 0;
+        (bool check,) = payable(msg.sender).call{value: money}("");
+        require(check);
+
     }
+
+    receive() payable external {}
 
 }
